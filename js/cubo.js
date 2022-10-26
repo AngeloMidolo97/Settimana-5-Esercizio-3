569673b6a16d1d971f04c79ddc3cb3725cb48342
AngeloMidolo97 <angelo.midolo97@gmail.com>
2022-10-26 17:48:49 +0200

    first commit

---
+++ js/cubo.js
@@ -0,0 +1,60 @@
+var degree = 0;
+
+const rotate = ()=> {
+    const cubes = document.querySelectorAll('.cube');
+    cubes.forEach(cube => cube.style.transform = `rotateY(${degree}deg)`);
+}
+
+//LEFT ARROW
+document.querySelector('.left-arrow').addEventListener('mouseover',() => {
+    degree += -25;
+    rotate();
+});
+
+document.querySelector('.left-arrow').addEventListener('mouseout',() => {
+    degree = 0;
+    rotate();
+});
+
+document.querySelector('.left-arrow').addEventListener('click',() => {
+    degree += -65;
+    rotate();
+    document.querySelector('.left-arrow').addEventListener('mouseout',() => {
+        degree += -90;
+        rotate();
+    });
+});
+
+//RIGHT ARROW
+document.querySelector('.right-arrow').addEventListener('mouseover',() => {
+    degree += 25;
+    rotate();
+});
+
+document.querySelector('.right-arrow').addEventListener('mouseout',() => {
+    degree = 0;
+    rotate();
+});
+
+document.querySelector('.right-arrow').addEventListener('click',() => {
+    degree += 65;
+    rotate();
+    document.querySelector('.right-arrow').addEventListener('mouseout',() => {
+        degree += 90;
+        rotate();
+    });
+});
+
+//PLAY-PAUSE BUTTON
+
+var toggle = true;
+
+document.querySelector('.play-pause').addEventListener('click',() => {
+
+    if (toggle == true) {toggle = false} else {toggle = true};
+
+    while(toggle == false) {
+        degree += 90;
+        rotate()++;
+    }
+});
